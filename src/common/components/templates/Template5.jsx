7652903b94d79ec3ea50bf809bf31e71cc6fd837
DRinsvind <adskiyprikol@gmail.com>
2019-10-01 13:56:56 +0300

    BN4 create templates of signature + copy function to this flow

---
+++ src/common/components/templates/Template5.jsx
@@ -0,0 +1,181 @@
+import React from 'react'
+
+export default function Template5(props) {
+    return (
+        <table id="template5" cellPadding={0} cellSpacing={0} style={{verticalAlign: '-webkit-baseline-middle', fontSize: 'small', fontFamily: 'Arial', width: '382px'}}>
+            <tbody>
+            <tr>
+                <td>
+                    <table cellPadding={0} cellSpacing={0} style={{verticalAlign: '-webkit-baseline-middle', fontSize: 'small', fontFamily: 'Arial', width: '100%'}}>
+                        <tbody>
+                        <tr>
+                            <td>
+                                <h2 color="#000000" style={{margin: '0px 0px 5px', fontSize: '20px', color: 'rgb(0, 0, 0)'}}>
+                                    <span>John</span><span>&nbsp;</span><span>Smith</span></h2>
+                                <p color="#000000" style={{margin: '0px 0px 5px', fontSize: '14px', color: 'rgb(0, 0, 0)'}}>
+                                    <span>Sales &amp; Marketing Director</span></p>
+                                <p color="#000000" style={{margin: '0px', fontSize: '14px', color: 'rgb(151, 151, 151)'}}>
+                                    <span>Finty | Sales department</span></p>
+                            </td>
+                            <td style={{textAlign: 'right'}}>
+                      <span style={{display: 'inline-block', textAlign: 'right', width: '100%'}}>
+                        <img width={90} src="https://avatarfiles.alphacoders.com/203/203431.jpg" role="presentation" style={{maxWidth: '130px', display: 'inline', textAlign: 'right'}} />
+                      </span>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td height={15} />
+            </tr>
+            <tr>
+                <td height={2} color="#d2245d" direction="horizontal" style={{width: '100%', borderBottom: '1px solid rgb(252, 165, 75)', borderLeft: 'none', display: 'block'}} />
+            </tr>
+            <tr>
+                <td height={23} />
+            </tr>
+            <tr>
+                <td style={{verticalAlign: 'middle'}}>
+                    <table cellPadding={0} cellSpacing={0} style={{verticalAlign: '-webkit-baseline-middle', fontSize: 'small', fontFamily: 'Arial'}}>
+                        <tbody>
+                        <tr>
+                            <td>
+                                <table cellPadding={0} cellSpacing={0} style={{verticalAlign: '-webkit-baseline-middle', fontSize: 'small', fontFamily: 'Arial'}}>
+                                    <tbody>
+                                    <tr height={25} style={{verticalAlign: 'middle'}}>
+                                        <td width={30} style={{verticalAlign: 'middle'}}>
+                                            <table cellPadding={0} cellSpacing={0} style={{verticalAlign: '-webkit-baseline-middle', fontSize: 'small', fontFamily: 'Arial'}}>
+                                                <tbody>
+                                                <tr>
+                                                    <td style={{verticalAlign: 'bottom'}}>
+                                      <span width={11} color="#d2245d" style={{display: 'block', backgroundColor: 'rgb(252, 165, 75)'}}>
+                                        <img width={13} src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/address-icon-2x.png" color="#d2245d" style={{display: 'block', backgroundColor: 'rgb(252, 165, 75)'}} />
+                                      </span>
+                                                    </td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                        <td style={{padding: '0px'}}>
+                                            <span color="#000000" style={{fontSize: '12px', color: 'rgb(0, 0, 0)'}}><span>Address 1, Address 2, Address 3, Address 4</span></span>
+                                        </td>
+                                    </tr>
+                                    <tr height={0} />
+                                    <tr height={25} style={{verticalAlign: 'middle'}}>
+                                        <td width={30} style={{verticalAlign: 'middle'}}>
+                                            <table cellPadding={0} cellSpacing={0} style={{verticalAlign: '-webkit-baseline-middle', fontSize: 'small', fontFamily: 'Arial'}}>
+                                                <tbody>
+                                                <tr>
+                                                    <td style={{verticalAlign: 'bottom'}}>
+                                      <span width={11} color="#d2245d" style={{display: 'block', backgroundColor: 'rgb(252, 165, 75)'}}>
+                                        <img width={13} src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/phone-icon-2x.png" color="#d2245d" style={{display: 'block', backgroundColor: 'rgb(252, 165, 75)'}} /></span>
+                                                    </td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                        <td style={{padding: '0px', color: 'rgb(0, 0, 0)'}}><a href="tel:+380966365260" color="#000000" style={{textDecoration: 'none', color: 'rgb(0, 0, 0)', fontSize: '12px'}}><span>111 222 3333</span></a>
+                                            | <a href="tel:+380966365260" color="#000000" style={{textDecoration: 'none', color: 'rgb(0, 0, 0)', fontSize: '12px'}}><span>111 222 3333</span></a>
+                                        </td>
+                                    </tr>
+                                    <tr height={0} />
+                                    <tr height={25} style={{verticalAlign: 'middle'}}>
+                                        <td width={30} style={{verticalAlign: 'middle'}}>
+                                            <table cellPadding={0} cellSpacing={0} style={{verticalAlign: '-webkit-baseline-middle', fontSize: 'small', fontFamily: 'Arial'}}>
+                                                <tbody>
+                                                <tr>
+                                                    <td style={{verticalAlign: 'bottom'}}><span width={11} color="#d2245d" style={{display: 'block', backgroundColor: 'rgb(252, 165, 75)'}}><img width={13} src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/email-icon-2x.png" color="#d2245d" style={{display: 'block', backgroundColor: 'rgb(252, 165, 75)'}} /></span>
+                                                    </td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                        <td style={{padding: '0px'}}>
+                                            <a href="#" color="#FA6400" style={{textDecoration: 'none', color: '#FA6400', fontSize: '12px'}}>
+                                                <span>john@smith.com</span>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <tr height={0} />
+                                    <tr height={25} style={{verticalAlign: 'middle'}}>
+                                        <td width={30} style={{verticalAlign: 'middle'}}>
+                                            <table cellPadding={0} cellSpacing={0} style={{verticalAlign: '-webkit-baseline-middle', fontSize: 'small', fontFamily: 'Arial'}}>
+                                                <tbody>
+                                                <tr>
+                                                    <td style={{verticalAlign: 'bottom'}}><span width={11} color="#d2245d" style={{display: 'block', backgroundColor: 'rgb(252, 165, 75)'}}><img width={13} src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/link-icon-2x.png" color="#d2245d" style={{display: 'block', backgroundColor: 'rgb(252, 165, 75)'}} /></span>
+                                                    </td>
+                                                </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                        <td style={{padding: '0px'}}>
+                                            <a href="#" color="#FA6400" style={{textDecoration: 'none', color: '#FA6400', fontSize: '12px'}}>
+                                                <span>www.hubspot.com</span>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td height={16} />
+            </tr>
+            <tr>
+                <td>
+                    <table cellPadding={0} cellSpacing={0} style={{verticalAlign: '-webkit-baseline-middle', fontSize: 'small', fontFamily: 'Arial', width: '100%'}}>
+                        <tbody>
+                        <tr>
+                            <td style={{verticalAlign: 'middle', textAlign: 'left'}}>
+                                <table cellPadding={0} cellSpacing={0} style={{verticalAlign: '-webkit-baseline-middle', fontSize: 'small', fontFamily: 'Arial', display: 'inline-block'}}>
+                                    <tbody>
+                                    <tr style={{textAlign: 'right'}}>
+                                        <td>
+                                            <a href="//23r23r" color="#6A78D1" style={{display: 'inline-block', padding: '0px', backgroundColor: 'rgb(106, 120, 209)'}}><img height={24} src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/facebook-icon-2x.png" alt="facebook" color="#6A78D1" style={{backgroundColor: 'rgb(106, 120, 209)', maxWidth: '135px', display: 'block'}} /></a>
+                                        </td>
+                                        <td width={5}>
+                                            <div />
+                                        </td>
+                                        <td>
+                                            <a href="//23r23r" color="#6A78D1" style={{display: 'inline-block', padding: '0px', backgroundColor: 'rgb(106, 120, 209)'}}><img height={24} src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/twitter-icon-2x.png" alt="twitter" color="#6A78D1" style={{backgroundColor: 'rgb(106, 120, 209)', maxWidth: '135px', display: 'block'}} /></a>
+                                        </td>
+                                        <td width={5}>
+                                            <div />
+                                        </td>
+                                        <td>
+                                            <a href="//23r23r" color="#6A78D1" style={{display: 'inline-block', padding: '0px', backgroundColor: 'rgb(106, 120, 209)'}}><img height={24} src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/linkedin-icon-2x.png" alt="linkedin" color="#6A78D1" style={{backgroundColor: 'rgb(106, 120, 209)', maxWidth: '135px', display: 'block'}} /></a>
+                                        </td>
+                                        <td width={5}>
+                                            <div />
+                                        </td>
+                                        <td>
+                                            <a href="//23r" color="#6A78D1" style={{display: 'inline-block', padding: '0px', backgroundColor: 'rgb(106, 120, 209)'}}><img height={24} src="https://cdn2.hubspot.net/hubfs/53/tools/email-signature-generator/icons/instagram-icon-2x.png" alt="instagram" color="#6A78D1" style={{backgroundColor: 'rgb(106, 120, 209)', maxWidth: '135px', display: 'block'}} /></a>
+                                        </td>
+                                        <td width={5}>
+                                            <div />
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                            <td style={{verticalAlign: 'top', textAlign: 'right'}}>
+                                <img width={90} src="https://brandongaille.com/wp-content/uploads/2013/07/Vans-Company-Logo.jpg" role="presentation" style={{maxWidth: '90px', display: 'inline', textAlign: 'left'}} />
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td height={30} />
+            </tr>
+            </tbody>
+        </table>
+    )
+}
