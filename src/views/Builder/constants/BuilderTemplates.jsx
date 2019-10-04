9277ff73f913b1842b5f65efe96d886262c3fc43
Rad Pozniakov <rad@pozniakov.com>
2019-10-04 16:29:37 +0300

    replaced email templates, builder content view is done
    d

---
+++ src/views/Builder/constants/BuilderTemplates.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+import Template1 from "../templates/Template1"
+import Template2 from "../templates/Template2"
+import Template3 from "../templates/Template3"
+import Template4 from "../templates/Template4"
+import Template5 from "../templates/Template5"
+import Template6 from "../templates/Template6"
+
+export const BUILDER_TEMPLATES = [
+    {
+        id: "template1",
+        component: Template1
+    },
+    {
+        id: "template2",
+        component: Template2
+    },
+    {
+        id: "template3",
+        component: Template3
+    },
+    {
+        id: "template4",
+        component: Template4
+    },
+    {
+        id: "template5",
+        component: Template5
+    },
+    {
+        id: "template6",
+        component: Template6
+    }
+]
